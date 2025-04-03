966fca0ae6799c1d49e418cfe22e337a0704fa57
MichaelRogers1978 <mikeangie3049@yahoo.com>
2025-04-02 22:55:38 -0700

    working website

--- script.js
+++ script.js
@@ -1,24 +1,18 @@
-/* Working JavaScript for Accordian */
-let acc = document.getElementsByClassName("accordion");
-let i;
 
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-        this.classList.toggle("active");
 
-        let panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
 
 
 /* Example JavaScript */
 function validateEmail(email) {
     return email.includes("@") && email.includes(".com");
   }
-  console.log(validateEmail("test@example.com")); // true
+  console.log(validateEmail("test@example.com")); 
+
+
+  
+let hasCoupon = true;
+let isSenior = false;
+if (hasCoupon || isSenior) {
+    console.log("Gets a discount!")
+}
 
