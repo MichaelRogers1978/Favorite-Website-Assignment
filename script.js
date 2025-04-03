0e4bbe2a1d2bbd8c284deb5a8ec2dbd7746d55b3
MichaelRogers1978 <mikeangie3049@yahoo.com>
2025-04-03 14:36:07 -0700

    Favorite Website Assignment

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-
-
-
-
 /* Example JavaScript */
 function validateEmail(email) {
-    return email.includes("@") && email.includes(".com");
-  }
-  console.log(validateEmail("test@example.com")); 
+  return email.includes("@") && email.includes(".com");
+}
+console.log(validateEmail("test@example.com"));
 
 
-  
 let hasCoupon = true;
 let isSenior = false;
 if (hasCoupon || isSenior) {
-    console.log("Gets a discount!")
+  console.log("Gets a discount!")
 }
 
+
+let cards = ["Hiking", "Camping", "Biking", "Climbing"];
+cards.push("Fishing");
+console.log(cards);
+
+
+let word = "Natures";
+console.log(word(0));
+console.log(word(3));
+console.log(word(6));
+
+
+let images = ["polynesia", "winter", "tents", "alpine-skiier", "cave", "scooter"];
+for (let i = 0; i < images.length; i++) {
+  console.log(`${i + 1}. ${images[i]}`);
+}
+for (let image of images) {
+  console.log(`image: ${image}`);
+}
+for (let index in tasks) {
+  console.log(`index: ${index}, image: ${images[index]}`);
+}
